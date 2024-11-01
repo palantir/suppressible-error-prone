776c497ab03a2863a755051485bfd5f1b6b8ca34
Callum Rogers <callumr@palantir.com>
2024-11-01 12:59:58 +0000

    Only set up the artifact transform when suppressing

--- gradle-suppressible-error-prone/src/main/java/com/palantir/gradle/suppressibleerrorprone/SuppressibleErrorPronePlugin.java
+++ gradle-suppressible-error-prone/src/main/java/com/palantir/gradle/suppressibleerrorprone/SuppressibleErrorPronePlugin.java
@@ -68,7 +68,10 @@ public final class SuppressibleErrorPronePlugin implements Plugin<Project> {
         // an unpatched version of errorprone that applies a single errorprone check: SuppressWarningsCoalesce,
         // which will combine all the @RepeatableSuppressWarnings and @SuppressWarnings annotations into one
         // normal @SuppressWarnings annotation.
-        setupErrorProneArtifactTransform(project);
+
+        if (isSuppressingStageOne(project)) {
+            setupErrorProneArtifactTransform(project);
+        }
 
         project.getConfigurations().named(ErrorPronePlugin.CONFIGURATION_NAME).configure(errorProneConfiguration -> {
             // Required so that we can run the runtime parts of the errorprone patching in suppressing stage 1 and
