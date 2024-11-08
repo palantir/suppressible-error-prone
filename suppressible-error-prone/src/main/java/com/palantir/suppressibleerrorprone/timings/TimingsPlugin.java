1cc8643368751cd1ebf497fffc52f42f402abd53
Callum Rogers <callumr@palantir.com>
2024-11-08 14:13:31 +0000

    package

--- suppressible-error-prone/src/main/java/com/palantir/suppressibleerrorprone/timings/TimingsPlugin.java
+++ suppressible-error-prone/src/main/java/com/palantir/suppressibleerrorprone/timings/TimingsPlugin.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.palantir.suppressibleerrorprone;
+package com.palantir.suppressibleerrorprone.timings;
 
 import com.google.auto.service.AutoService;
 import com.google.errorprone.ErrorProneTimings;
